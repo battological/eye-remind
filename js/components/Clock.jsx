38862ade6aee6f21dad79ee918a27153c4e7fa2e
gtsherman <backwardstg@gmail.com>
2015-10-03 00:01:49 -0500

    more clock improvements

--- js/components/Clock.jsx
+++ js/components/Clock.jsx
@@ -1,9 +1,18 @@
 var Clock = React.createClass({
+  getDefaultProps() {
+    return({
+      lookAwayMsg: 'Time for an eye break!',
+      lookBackMsg: 'You can look back, now!',
+      screenClockTime: 20*60*1000,  // 20 min * 60 sec * 1000 millisec
+      awayClockTime: 20*1000  // 20 sec * 1000 millisec
+    });
+  },
+
   getInitialState() {
     return ({
-      clockTime: 20*60*1000,  // 20 minutes * 60 seconds * 1000 milliseconds
-      clock: '20:00',
-      lookAway: 'Time for an eye break!'
+      currentClockTime: this.props.screenClockTime,
+      currentClock: this.formatMilliseconds(this.props.screenClockTime),
+      mode: 1  // 1 for screen, 0 for away
     });
   },
 
@@ -14,27 +23,20 @@ var Clock = React.createClass({
    */
   countdown(time, text) {
     // Update clock if time differs from current state
-    if (time != this.state.clockTime) {
-      this.setState({ 
-        clockTime: time,
-	clock: this.formatMilliseconds(time)
-      });
+    if (time != this.state.currentClockTime) {
+      this.updateClock(time);
     }
 
     // Countdown the clock by 1 second
     setTimeout(function() {
-      var curTime = this.state.clockTime;
-      var newTime = curTime-1000;
-      this.setState({ 
-        clockTime: newTime,
-	clock: this.formatMilliseconds(newTime)
-      });
+      var newTime = this.state.currentClockTime-1000;
+      this.updateClock(newTime);
 
       // If the clock has reached zero, sound the alarm
-      if (!this.state.clockTime) {
+      if (!this.state.currentClockTime) {
         this.soundAlarm(text);
       } else {  // Otherwise repeat
-        this.countdown(this.state.clockTime, text);
+        this.countdown(this.state.currentClockTime, text);
       }
     }.bind(this), 1000);
   },
@@ -47,6 +49,36 @@ var Clock = React.createClass({
     var alarm = new Audio('public/audio/alert.wav');
     alarm.play();
     alert(text);
+
+    
+    this.toggleMode();
+    if (!this.state.mode) {  // If we just ended screen mode
+      this.countdown(this.state.currentClockTime, this.props.lookBackMsg);
+    } else {  // If we just ended away mode
+      this.countdown(this.state.currentClockTime, this.props.lookAwayMsg);
+    }
+  },
+
+  toggleMode() {
+    this.setState({ mode: !this.state.mode });
+
+    // Set the clock time and the clock for the new mode
+    var cct = this.props.screenClockTime;
+    if (!this.state.mode) {
+      cct = this.props.awayClockTime;
+    }
+    this.updateClock(cct);
+  },
+    
+  /**
+   * Updates the state of the clock
+   * @param {number} newClockTime - The new clock time in milliseconds
+   */
+  updateClock(newClockTime) {
+    this.setState({ 
+      currentClockTime: newClockTime,
+      currentClock: this.formatMilliseconds(newClockTime)
+    });
   },
 
   /**
@@ -56,18 +88,21 @@ var Clock = React.createClass({
    */
   formatMilliseconds(ms) {
     var d = new Date(ms);
-    return d.getMinutes()+':'+d.getSeconds();
+    var hour = (d.getHours() < 9 ? '0' : '')+d.getHours();
+    var min = (d.getMinutes() <= 9 ? '0' : '')+d.getMinutes();
+    var sec = (d.getSeconds() <= 9 ? '0' : '')+d.getSeconds();
+    return min+':'+sec;
   },
 
   render() {
     return(
       <div className="clock">
         <span className="clock">
-	  {this.state.clock}
+	  {this.state.currentClock}
 	</span>
         <button 
 	  className="start-clock" 
-	  onClick={this.countdown.bind(this, this.state.clockTime, this.state.lookAway)}>
+	  onClick={this.countdown.bind(this, this.state.currentClockTime, this.props.lookAwayMsg)}>
 	  Go
 	</button>
       </div>
